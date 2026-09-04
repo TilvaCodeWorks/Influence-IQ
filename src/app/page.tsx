0408862e1ae871e23146516a2af67a2f4c274161
DishaRSC <disha@rscgroupdholera.in>
2026-09-04 16:44:24 +0530

    Initial commit - InfluenceIQ Next.js project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ export default function Home() {
           className="dark:invert h-5 w-[100px]"
           src="/next.svg"
           alt="Next.js logo"
+
+          
           width={100}
           height={20}
           priority
